48abb9039707ad9372897df387104fb83fd6ce8d
DOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-07-09 12:58:02 +0530

    Create start.py

--- core/start.py
+++ core/start.py
@@ -1,19 +1,14 @@
 import asyncio
+
 from helpers.filters import command
+from config import BOT_NAME as bn, BOT_USERNAME as bu, SUPPORT_GROUP, OWNER_USERNAME as me, START_IMG
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from config import (
-    BOT_NAME as bn, START_IMG, BOT_USERNAME as bu, SUPPORT_GROUP,
-    OWNER_USERNAME as me)
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(
-    command("start") & filters.private & ~filters.group & ~filters.edited
-)
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_sticker(
-        "CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME"
-    )
+    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
     await message.reply_photo(
         photo=f"{START_IMG}",
         caption=f"""**━━━━━━━━━━━━━━━━━━
@@ -30,27 +25,27 @@ async def start_(client: Client, message: Message):
 
 💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴀʙᴏᴜᴛ ᴍᴇ ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](t.me/{me}) ʙᴀʙʏ...
 ━━━━━━━━━━━━━━━━━━**""",
-        reply_markup=InlineKeyboardMarkup(
+    reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
                         "🥺 ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ​ 🥺", url=f"https://t.me/{bu}?startgroup=true"
+                       ),
+                  ],[
+                    InlineKeyboardButton(
+                        "💔 ᴏᴡɴᴇʀ 💔", url=f"https://t.me/{me}"
                     ),
-                ],
-                [
-                    InlineKeyboardButton("💔 ᴏᴡɴᴇʀ 💔", url=f"https://t.me/{me}"),
                     InlineKeyboardButton(
                         "🍒 sᴜᴘᴘᴏʀᴛ 🍒", url=f"https://t.me/{SUPPORT_GROUP}"
-                    ),
-                ],
-                [
+                    )
+                ],[
                     InlineKeyboardButton(
                         "🔎 ɪɴʟɪɴᴇ 🔎", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "🤯 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ 🤯", url="https://github.com/dominator454/Music"
-                    ),
-                ],
+                        "🤯 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ 🤯", url="https://github.com/dominator454/Music_Bot"
+                    )]
             ]
-        ),
+       ),
     )
+
